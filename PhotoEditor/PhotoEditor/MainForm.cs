cad971002e68acb32aa0b3861b6f81508749d407
adamhammond <ahammond@harding.edu>
2015-09-26 23:41:48 -0500

    Completed MainForm.cs

--- PhotoEditor/PhotoEditor/MainForm.cs
+++ PhotoEditor/PhotoEditor/MainForm.cs
@@ -1,79 +1,110 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-// Authors: Justin Greenlee & Adam Hammond
-// Date: September 26th, 2015
-// Class: GUI
-// Assignment: Photo Editor
-
 namespace PhotoEditor
 {
     public partial class MainForm : Form
     {
-        // private variables
-        private string rootDirectory;
-        private string currentPath;
-        private FolderBrowserDialog folderBrowserDialog1;
-        private OpenFileDialog openFileDialog1;
-        private AboutForm aboutForm1;
-        private ImageList images;
-        private bool detailsView = false;
-
-        #region Constructor & Load
         public MainForm()
         {
             InitializeComponent();
-            folderBrowserDialog1 = new FolderBrowserDialog();
-            openFileDialog1 = new OpenFileDialog();
-            aboutForm1 = new AboutForm();
-            images = new ImageList();
-            images.ImageSize = new Size(32, 32);
+
+            // Change 16x16 (small) application icon (top-left corner of window)
+            this.Icon = Icon.FromHandle(PhotoEditor.Properties.Resources.PhotoEditorIcon.GetHicon());
+            aboutForm = new AboutForm(this);
+            AddFolderIconToDirectoryTreeViewNodes();
+            this.detailToolStripMenuItem.Click += viewToolStripMenuItem_Click;
+            this.smallToolStripMenuItem.Click += viewToolStripMenuItem_Click;
+            this.largeToolStripMenuItem.Click += viewToolStripMenuItem_Click;
         }
 
-        // main form load event
+        private HashSet<TreeNode> nodesVisited = new HashSet<TreeNode>();
+
+        private AboutForm aboutForm;
+
+        private void AddFolderIconToDirectoryTreeViewNodes()
+        {
+            // create an ImageList to hold our new Folder Icon
+            ImageList imageList = new ImageList();
+            imageList.ImageSize = new Size(16, 16);
+            imageList.ColorDepth = ColorDepth.Depth32Bit;
+            imageList.Images.Add(PhotoEditor.Properties.Resources.FolderIcon);
+
+            treeView1.ImageList = imageList;
+        }
+
+        private string rootDirectory;
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            rootDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-            ListDirectory(this.treeView1, rootDirectory);
+            if (!DesignMode)
+            {
+                toolStripProgressBar1.Visible = false;
+            }
+
+            ChangeRootFolder(new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)));
+
         }
-        #endregion
-        #region TreeView Event Handlers
+
+        private void ChangeRootFolder(DirectoryInfo rootDir)
+        {
+            nodesVisited.Clear();
+
+            var rootNode = new TreeNode(rootDir.Name);
+            rootNode.Tag = rootDir;
+
+            foreach (var dir in rootDir.GetDirectories())
+            {
+                var currentNode = new TreeNode(dir.Name);
+                currentNode.Tag = dir;
+
+                foreach (var subDir in dir.GetDirectories())
+                {
+                    var childNode = new TreeNode(subDir.Name);
+                    childNode.Tag = subDir;
+                    currentNode.Nodes.Add(childNode);
+                }
+
+                rootNode.Nodes.Add(currentNode);
+            }
+
+            rootNode.Expand();
+            nodesVisited.Add(rootNode);
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(rootNode);
+        }
+
+        private void locateOnDiskToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer", ((DirectoryInfo)treeView1.SelectedNode.Tag).FullName);
+        }
+
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            listView1.Clear();
-
-            // append selected path to current path
-            string path = currentPath = rootDirectory;
-            if (e.Node.Level != 0)
+            if (backgroundWorker1.IsBusy && backgroundWorker1.WorkerSupportsCancellation)
             {
-                string nodePath = e.Node.FullPath;
-                int i = nodePath.IndexOf("\\");
-                path += nodePath.Substring(i, nodePath.Length - i);
-                currentPath = path;
-            }
-
-            // initialize directory and image list
-            listView1.View = View.LargeIcon;
-            if (!backgroundWorker1.IsBusy)
-                backgroundWorker1.RunWorkerAsync();
-            else
-            {
-                if (backgroundWorker1.WorkerSupportsCancellation)
+                backgroundWorker1.CancelAsync();
+                while (backgroundWorker1.IsBusy)
                 {
-                    backgroundWorker1.CancelAsync(); // cancels and restart for new selection
+                    Application.DoEvents();
                 }
             }
+
+            toolStripProgressBar1.Visible = true;
+            backgroundWorker1.RunWorkerAsync(e.Node.Tag);
+
         }
-        #endregion
-        #region Directory Functions
+
         // Recursively fill the treeView with new DirectoryInfo Nodes
         private void ListDirectory(TreeView treeView, String path)
         {
@@ -86,183 +117,210 @@ namespace PhotoEditor
         private static TreeNode CreateDirectoryNode(DirectoryInfo parentDir)
         {
             var directoryNode = new TreeNode(parentDir.Name);
-            
+            directoryNode.Tag = parentDir;
+
             // Loop for Sub-directories
             foreach (var directory in parentDir.GetDirectories())
+            {
                 directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            }
 
             return directoryNode;
         }
-        #endregion
-        #region Menu Event Handlers
-        private void selectRootFolderToolStripMenuItem_Click(object sender, EventArgs e)
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-            // Show the FolderBrowserDialog.
-            DialogResult result = folderBrowserDialog1.ShowDialog();
-            if (result == DialogResult.OK)
+            AsyncUtilities.InvokeIfRequired(this, (o) =>
+           {
+               toolStripStatusLabel1.Text = "Working...";
+           });
+
+            AsyncUtilities.InvokeIfRequired(this, (o) =>
+                {
+                    o.listView1.Items.Clear();
+                    ImageList smallImageList = new ImageList();
+                    ImageList largeImageList = new ImageList();
+                    smallImageList.ImageSize = new Size(48, 48);
+                    largeImageList.ImageSize = new Size(128, 128);
+                    smallImageList.ColorDepth = ColorDepth.Depth32Bit;
+                    largeImageList.ColorDepth = ColorDepth.Depth32Bit;
+                    o.listView1.SmallImageList = smallImageList;
+                    o.listView1.LargeImageList = largeImageList;
+                });
+
+            DirectoryInfo dir = (DirectoryInfo)e.Argument;
+
+            var imageFilesInDir = new List<FileInfo>();
+
+            foreach (var file in dir.GetFiles())
             {
-                rootDirectory = folderBrowserDialog1.SelectedPath;
-                ListDirectory(treeView1, rootDirectory);
+                if (file.Extension.ToLower().Equals(".jpg") || file.Extension.ToLower().Equals(".jpeg"))
+                {
+                    imageFilesInDir.Add(file);
+                }
+            }
+
+            foreach (var file in imageFilesInDir)
+            {
+                if (backgroundWorker1.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                try
+                {
+                    var image = Image.FromFile(file.FullName);
+                    var item = new ListViewItem(
+                                new string[] { file.Name, file.LastWriteTime.ToString(), BytesToString(file.Length) },
+                                imageFilesInDir.IndexOf(file));
+                    item.Tag = file;
+
+                    AddItemToListView(item, image);
+                }
+                catch
+                {
+                    // do nothing
+                }
+
+
+
             }
         }
-        private void locateOnDiskToolStripMenuItem_Click(object sender, EventArgs e)
+
+        private void AddItemToListView(ListViewItem item, Image image)
         {
-            openFileDialog1.InitialDirectory = folderBrowserDialog1.SelectedPath;
-            openFileDialog1.FileName = null;
-            DialogResult result = openFileDialog1.ShowDialog();
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(() => AddItemToListView(item, image)));
+            }
+            else
+            {
+                listView1.SmallImageList.Images.Add(image);
+                listView1.LargeImageList.Images.Add(image);
+                listView1.Items.Add(item);
+            }
+        }
+
+        public static String BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+            toolStripProgressBar1.Visible = false;
+        }
+
+        private void selectRootFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog rootFolderDialog = new FolderBrowserDialog();
+            rootFolderDialog.ShowNewFolderButton = false;
+            DialogResult result = rootFolderDialog.ShowDialog(this);
+
+            //As long as the user didn't cancel, switch the root folder.
             if (result == DialogResult.OK)
             {
-                // Open Photo Editor for the selected file
+                ChangeRootFolder(new DirectoryInfo(rootFolderDialog.SelectedPath));
+                treeView1.SelectedNode = treeView1.Nodes[0];
             }
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Application.Exit();
-        }
-
-        private void detailToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            detailsView = true;
-            listView1.Clear();
-            listView1.View = View.Details;
-            listView1.Columns.Add("Name", 100);
-            listView1.Columns.Add("Date", 150);
-            listView1.Columns.Add("Size", 100);
-            listView1.Dock = DockStyle.Fill;
-            images.ImageSize = new Size(32, 32);
-            listView1.SmallImageList = images;
-
-            if (!backgroundWorker1.IsBusy)
-                backgroundWorker1.RunWorkerAsync();
-            else backgroundWorker1.CancelAsync();
-        }
-
-        private void smallToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            detailsView = false;
-            listView1.Clear();
-            listView1.View = View.SmallIcon;
-            images.ImageSize = new Size(32, 32);
-            listView1.SmallImageList = images;
-
-            if (!backgroundWorker1.IsBusy)
-                backgroundWorker1.RunWorkerAsync();
-            else backgroundWorker1.CancelAsync();
-        }
-
-        private void largeToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            detailsView = false;
-            listView1.Clear();
-            listView1.View = View.LargeIcon;
-            images.ImageSize = new Size(64, 64);
-            listView1.LargeImageList = images;
-
-            if (!backgroundWorker1.IsBusy)
-                backgroundWorker1.RunWorkerAsync();
-            else backgroundWorker1.CancelAsync();
+            Close();
         }
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            // Show the AboutForm.
-            DialogResult result = aboutForm1.ShowDialog();
-        }
-        #endregion
-        #region Background Worker Event Handling
-        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-            if (progressBar1.Value != progressBar1.Maximum)
-                progressBar1.Value++;
+            aboutForm.ShowDialog();
         }
 
-        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        private void viewToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (e.Cancelled)
-            {
-                listView1.Clear(); // clear and restart for new selection
-                if (!backgroundWorker1.IsBusy)
-                    backgroundWorker1.RunWorkerAsync();
-            }
+
         }
 
-        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
         {
-            BackgroundWorker worker = sender as BackgroundWorker;
-            var directory = new DirectoryInfo(currentPath);
-            SetImageList(images);
-
-            // Initialize Progress Bar, Count JPG Images in Directory
-            int imageCount = 0;
-            foreach (var file in directory.GetFiles())
+            if (e.Node.GetNodeCount(false) != 0 && !nodesVisited.Contains(e.Node))
             {
-                if (file.Extension == ".jpg") imageCount++;
-            }
-            InitializeProgressBar(imageCount);
-
-            // Loop through files in currently selected directory
-            int index = 0;
-            foreach (var file in directory.GetFiles())
-            {
-                if (worker.CancellationPending)
+                foreach (TreeNode dirNode in e.Node.Nodes)
                 {
-                    e.Cancel = true;
-                    break;
-                }
-                if (file.Extension == ".jpg")
-                {
-                    images.Images.Add(file.Name, Image.FromFile(file.FullName));
-                    if (!detailsView)
+                    DirectoryInfo dir = dirNode.Tag as DirectoryInfo;
+
+                    foreach (DirectoryInfo subdir in dir.GetDirectories())
                     {
-                        var item = new ListViewItem(file.Name, index);
-                        AppendItemToListView(item);
-                    } else
-                    {
-                        var details = new string[]  { file.Name,
-                                                      file.LastWriteTime.ToString(),
-                                                      file.Length.ToString() };
-                        var item = new ListViewItem(details, index);
-                        AppendItemToListView(item);
+                        TreeNode subdirNode = new TreeNode(subdir.Name);
+                        subdirNode.Tag = subdir;
+                        dirNode.Nodes.Add(subdirNode);
                     }
-                    worker.ReportProgress(1);
-                    index++;
+                }
+
+                nodesVisited.Add(e.Node);
+            }
+        }        
+
+        private void viewChildMenuItemClicked(object sender, EventArgs e)
+        {
+            ToolStripMenuItem viewMenuItem = sender as ToolStripMenuItem;
+
+            //Do nothing if the item was already selected.
+            if (viewMenuItem.CheckState == CheckState.Checked)
+            {
+                return;
+            }
+            
+            detailToolStripMenuItem.CheckState = CheckState.Unchecked;
+            smallToolStripMenuItem.CheckState = CheckState.Unchecked;
+            largeToolStripMenuItem.CheckState = CheckState.Unchecked;
+            viewMenuItem.CheckState = CheckState.Checked;
+
+            //Pass on the selected view mode to the imageListView.
+            if (viewMenuItem == detailToolStripMenuItem)
+            {
+                listView1.View = View.Details;
+            }
+            else if (viewMenuItem == smallToolStripMenuItem)
+            {
+                listView1.View = View.SmallIcon;
+            }
+            else if (viewMenuItem == largeToolStripMenuItem)
+            {
+                listView1.View = View.LargeIcon;
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //Get the item targetted by the double click.
+            ListViewItem selectedItem = listView1.GetItemAt(e.X, e.Y);
+
+            //Make sure the user actually targetted an item, then open the editor.
+            if (selectedItem != null)
+            {
+                FileInfo imageFile = selectedItem.Tag as FileInfo;
+                Image image = Image.FromFile(imageFile.FullName);
+                EditForm imageEditor = new EditForm(new Bitmap(image));
+                DialogResult result = imageEditor.ShowDialog(this);
+
+                //If the user hit the 'save' button, save the image to disk.
+                if (result == DialogResult.OK)
+                {
+                    imageEditor.CurrentImage.Save(imageFile.FullName, ImageFormat.Jpeg);
                 }
             }
         }
 
-        #endregion
-        #region Delegate Methods (UI stuff)
-        private void AppendItemToListView(ListViewItem item)
-        {
-            if (InvokeRequired)
-                Invoke(new MethodInvoker(() => AppendItemToListView(item)));
-            else
-                listView1.Items.Add(item);
-        }
-        public void SetImageList(ImageList images)
-        {
-            if (InvokeRequired)
-                Invoke(new MethodInvoker(() => SetImageList(images)));
-            else
-            {
-                images.Images.Clear();
-                listView1.SmallImageList = images;
-                listView1.LargeImageList = images;
-            }    
-        }
-        public void InitializeProgressBar(int max)
-        {
-            if (InvokeRequired)
-                Invoke(new MethodInvoker(() => InitializeProgressBar(max)));
-            else
-            {
-                progressBar1.Minimum = 0;
-                progressBar1.Maximum = max;
-                progressBar1.Visible = true;
-            }
-        }
-        #endregion
+
+
 
     }
 }
